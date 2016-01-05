57526fbc3e28e7392de65e95b63cb626f30553bd
Ryan Wadsworth <ryanrain@gmail.com>
2016-01-05 15:08:55 -0600

    add bootstrap mobile menu toggle and dropdown

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,27 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
+
+    <div class="navbar-header">
+      
+      <button class="navbar-toggler hidden-md-up" type="button" data-toggle="collapse" data-target="#primary-nav">
+        &#9776;
+      </button>
+      
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+
+    </div>
+
+    <nav class="collapse navbar-toggleable-sm" role="navigation" id="primary-nav">
+      <?php      
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu([
+          'theme_location' => 'primary_navigation', 
+          'walker' => new wp_bootstrap_navwalker(), 
+          'menu_class' => 'nav navbar-nav'
+        ]);
       endif;
       ?>
     </nav>
+
   </div>
 </header>
