e06c51c570740481a4a2c183b55b5899ec982e6f
Ryan Wadsworth <ryanrain@gmail.com>
2016-01-18 10:29:19 -0600

    add markup and icon for search form

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+		<input type="search" class="search-field" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+	</label>
+<button type="submit" class="search-submit"/>
+		<?php include('dist/images/icons/search.svg'); ?>
+	</button>
+</form>
